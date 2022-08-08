c765f3e105a8cb0b40a47020069366d2ca1f8660
henriqueespadas <henriqueep@hotmail.com>
2022-08-07 22:48:33 -0300

    add web_site_sac

---
+++ meusmodulos/web_site_sac/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Eva Alexandrina Tecnologia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Web Site Sac',
+    'summary': """
+        Sac website""",
+    'version': '13',
+    'license': 'AGPL-3',
+    'author': 'Eva Alexandrina Tecnologia,Odoo Community Association (OCA)',
+    'depends': [
+        'sac',
+        'website_form'
+    ],
+    'data': [
+        'views/website_form.xml'
+    ],
+    'demo': [
+    ],
+}
